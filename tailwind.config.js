2d8b0b682fbdc1069a237bf2fa54a7dd70539950
rahmanbax <basyirahman@gmail.com>
2025-01-05 15:18:34 +0700

    tailwind version update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
@@ -8,24 +7,11 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
->>>>>>> 1bb71edf16ce6da59b21651bdf421317d768b6e8
     ],
 
     theme: {
       extend: {},
     },
-<<<<<<< HEAD
 
     plugins: [forms],
 };
-=======
-    plugins: [],
-  }
->>>>>>> 1bb71edf16ce6da59b21651bdf421317d768b6e8
