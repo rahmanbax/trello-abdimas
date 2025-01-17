710b88300c30d350d500d6c9dbe31687f6aad2e8
rahmanbax <basyirahman@gmail.com>
2025-01-17 16:05:23 +0700

    no middleware for route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/project', function () {
     return view('project.index');
-});
+})->middleware('auth');
 
 Route::get('/project/{id}', function ($id) {
     return view('project.detail', ['id' => $id]);
