7cd196e50682e934d982ae276f6c32eb51234cb5
Bagas Permana <wolfieschewstore91@gmail.com>
2025-11-04 12:06:08 +0700

    fix: Fix Conflicts

--- public/task.js
+++ public/task.js
@@ -1,4 +1,4 @@
-//  const API_BASE_URL = "https://trelloapp.id/api";
+// const API_BASE_URL = "https://trelloapp.id/api";
 const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 const token = localStorage.getItem("access_token"); // Mengambil token
