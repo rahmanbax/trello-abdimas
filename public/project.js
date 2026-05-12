420afeda9ab5eb3bb728412c0f6ddbaf151d6308
Bagas Permana <wolfieschewstore91@gmail.com>
2026-05-12 11:32:05 +0700

    Update API base URL to production in JavaScript and Blade files; modify asset file names in manifest.json

--- public/project.js
+++ public/project.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://trelloapp.id/api";
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "https://trelloapp.id/api";
+// const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 document.addEventListener("DOMContentLoaded", async function () {
     const apiEndpoint = `${API_BASE_URL}/projects`;
